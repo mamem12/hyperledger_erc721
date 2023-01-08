05306b23c02b8456bf76e41db374864c267032ea
mamem12 <mamem.dev@gmail.com>
2023-01-08 16:29:53 +0900

    레포지토리 재설정

---
+++ main.go
@@ -0,0 +1,35 @@
+/*
+ */
+
+package main
+
+import (
+	"hyperledger_erc721/chaincode"
+
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"github.com/hyperledger/fabric-contract-api-go/metadata"
+)
+
+func main() {
+	nftContract := new(chaincode.TokenERC721Contract)
+	nftContract.Info.Version = "0.0.1"
+	nftContract.Info.Description = "ERC-721 fabric port"
+	nftContract.Info.License = new(metadata.LicenseMetadata)
+	nftContract.Info.License.Name = "Apache-2.0"
+	nftContract.Info.Contact = new(metadata.ContactMetadata)
+	nftContract.Info.Contact.Name = "Matias Salimbene"
+
+	chaincode, err := contractapi.NewChaincode(nftContract)
+	chaincode.Info.Title = "ERC-721 chaincode"
+	chaincode.Info.Version = "0.0.1"
+
+	if err != nil {
+		panic("Could not create chaincode from TokenERC721Contract." + err.Error())
+	}
+
+	err = chaincode.Start()
+
+	if err != nil {
+		panic("Failed to start chaincode. " + err.Error())
+	}
+}
